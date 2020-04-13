a3e858d56028c383f388f6057c16da0f6f0697d0
Nikita Chigvintsev <nikmail007@gmail.com>
2020-04-13 13:45:51 +0700

    Init sprites in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,27 @@
 #include <ctime>
 #include <iostream>
 const int n = 5;
+#define m 192
 using namespace std;
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(m, m), "test. kursach-TRPO");
+
+    Image numbersimage, othersimage;
+    numbersimage.loadFromFile("Numbers.jpg");
+    othersimage.loadFromFile("Others.jpg");
+
+    Texture numberstexture, otherstexture;
+    numberstexture.loadFromImage(numbersimage);
+    otherstexture.loadFromImage(othersimage);
+
+    Sprite numberssprite, scoresprite, restartsprite;
+    numberssprite.setTexture(numberstexture);
+    scoresprite.setTexture(otherstexture);
+    restartsprite.setTexture(otherstexture);
+
     srand(time(0));
 
     int** arr = new int*[n];
