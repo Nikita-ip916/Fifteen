6648d9607ee68b39a0e54e4d3ce11d68edca2892
Nikita Chigvintsev <nikmail007@gmail.com>
2020-04-17 19:22:33 +0700

    Added output of time in hh:mm:ss format

--- src/main.cpp
+++ src/main.cpp
@@ -47,12 +47,20 @@ int main()
     }
     generateArray(arr, n);
     cout << endl;
-
+    int timem = 0, timeh = 0;
     while (window.isOpen()) {
         Vector2i pos = Mouse::getPosition(window);
         int x = pos.x / 32;
         int y = pos.y / 32;
         int time = clock.getElapsedTime().asSeconds();
+        if (time > 59) {
+            clock.restart();
+            timem++;
+        }
+        if (timem > 59) {
+            timem = 0;
+            timeh++;
+        }
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed) {
                 window.close();
@@ -75,10 +83,11 @@ int main()
                 window.draw(nmbr);
             }
         }
-        ostringstream Sec;
-        Sec << setfill('0') << setw(2) << time;
-        text.setString(Sec.str());
-        text.setPosition(0, 0);
+        ostringstream Out;
+        Out << setfill('0') << setw(2) << timeh << ":" << setfill('0')
+            << setw(2) << timem << ":" << setfill('0') << setw(2) << time;
+        text.setString(Out.str());
+        text.setPosition(32, 0);
         scr.setPosition(32, 160);
         rstrt.setPosition(160, 128);
         window.draw(text);
