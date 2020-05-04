d78bb985e8cdd130de6baae64cb28af7bf93bec7
Nikita Chigvintsev <nikmail007@gmail.com>
2020-05-04 17:36:23 +0700

    Created tests of horizontal moving

--- test/MoveTest.h
+++ test/MoveTest.h
@@ -33,4 +33,63 @@ public:
             }
         }
     }
+    void test_left_wrong(void)
+    {
+        dir = 1;
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                arr[i][j] = j + 4 * (i - 1) - 1;
+                exp[i][j] = j + 4 * (i - 1) - 1;
+            }
+        }
+        arr[1][1] = exp[1][1] = arr[1][4];
+        arr[1][4] = exp[1][4] = 0;
+        mas[0] = 1;
+        mas[1] = 4;
+        moveF(dir, arr, n, mas);
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                TS_ASSERT_EQUALS(exp[i][j], arr[i][j]);
+            }
+        }
+    }
+    void test_right_correct(void)
+    {
+        dir = 2;
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                arr[i][j] = j + 4 * (i - 1) - 1;
+                exp[i][j] = j + 4 * (i - 1) - 1;
+            }
+        }
+        arr[1][1] = exp[1][1] = arr[2][3];
+        arr[2][3] = exp[2][3] = 0;
+        mas[0] = 2;
+        mas[1] = 3;
+        exp[2][3] = arr[2][2];
+        exp[2][2] = 0;
+        moveF(dir, arr, n, mas);
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                TS_ASSERT_EQUALS(exp[i][j], arr[i][j]);
+            }
+        }
+    }
+    void test_right_wrong(void)
+    {
+        dir = 2;
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                arr[i][j] = j + 4 * (i - 1) - 1;
+                exp[i][j] = j + 4 * (i - 1) - 1;
+            }
+        }
+        mas[0] = mas[1] = 1;
+        moveF(dir, arr, n, mas);
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < n; j++) {
+                TS_ASSERT_EQUALS(exp[i][j], arr[i][j]);
+            }
+        }
+    }
 };
