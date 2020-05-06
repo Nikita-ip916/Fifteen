c16303843363ed03e420788e91f96e8091d0774d
JustQuas <xfancytown@mail.ru>
2020-05-07 00:47:10 +0700

    Changed the method to read into the struct

--- src/leaderboard.cpp
+++ src/leaderboard.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string>
 using namespace std;
@@ -13,7 +14,7 @@ struct result {
 
 void getResult(int* t)
 {
-    fstream records("records.txt", ios::app | fstream::out);
+    fstream records("records.txt", ios::app | fstream::out | fstream::in);
     string name;
     if (records) {
         do {
@@ -33,44 +34,56 @@ void getResult(int* t)
 
 void sortResult()
 {
-    fstream records("records.txt", ios::app | fstream::out);
+    fstream records("records.txt", ios::app | fstream::out | fstream::in);
     int count = 0;
-    string name;
-    char* str = new char[512];
-    /* Подсчёт строк.
-     *
-     * Проблема: считывает до символа '\n', а не до конца файла.
-     *
-     */
-    while (!records.eof()) {
-        records.getline(str, 512, '\n');
+    char* str = new char[500];
+    while (records) {
+        records.getline(str, 500);
         count++;
     }
-    delete str;
+    delete[] str;
+    count--; // Убираем фактически пустую строку.
 
     result* data = new result[count];
+    string buffer;
     for (int i = 0; i < count; i++) {
-        records >> r.name >> r.h >> r.m >> r.s;
+        getline(records, buffer);
+        r.name = buffer;
+        buffer.clear();
+        // Т.к. getline считывает до пробела, то используем getline.
+        getline(records, buffer);
+        // atoi - переводит из string в int.
+        r.h = atoi(buffer.c_str());
+        getline(records, buffer);
+        r.s = atoi(buffer.c_str());
+        buffer.clear();
+        getline(records, buffer);
+        r.s = atoi(buffer.c_str());
+        buffer.clear();
         data[i] = r;
+        // Но выводит всё равно нули, а имя - пустое.
+        cout << data[i].name << data[i].h << data[i].m << data[i].s << endl;
     }
-    cout << endl;
-    for (int i = 0; i < count; i++) {
-        cout << data[i].name << data[i].h << data[i].m
-             << data[i].s; // Выводит нули, имя - пустое. Вывод: Не записывается
-                           // в структуру
-    }
-
     cout << endl;
     // Здесь сортировка будет, но сначала решить проблему с записью в структуру.
     for (int i = 0; i < count; i++) {
         for (int j = 0; j < count; j++) {
             if (data[i].h > data[j].h) {
+                swap(data[i].name, data[j].name);
                 swap(data[i].h, data[j].h);
+                swap(data[i].m, data[j].m);
+                swap(data[i].s, data[j].s);
             } else {
                 if (data[i].m > data[j].m) {
+                    swap(data[i].name, data[j].name);
+                    swap(data[i].h, data[j].h);
                     swap(data[i].m, data[j].m);
+                    swap(data[i].s, data[j].s);
                 } else {
                     if (data[i].s > data[j].s) {
+                        swap(data[i].name, data[j].name);
+                        swap(data[i].h, data[j].h);
+                        swap(data[i].m, data[j].m);
                         swap(data[i].s, data[j].s);
                     }
                 }
@@ -78,21 +91,6 @@ void sortResult()
         }
     }
     delete[] data;
-    // Здесь будем убирать лишние результаты.
-    if (count > 5) {
-        result* newData = new result[5];
-        for (int i = 0; i < count; i++) {
-            cin >> r.name >> r.h >> r.m >> r.s;
-            data[i] = r;
-        }
-        records.close();
-        fstream records("records.txt", ios::trunc);
-        for (int i = 0; i < count; i++) {
-            records << data[i].name << data[i].h << data[i].m << data[i].s
-                    << '\n';
-        }
-        delete[] newData;
-    }
     records.close();
 }
 // Просто для тестов.
