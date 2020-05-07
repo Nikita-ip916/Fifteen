2630fd9a79cb8560a7c40eff2aad1a5a9fe28617
Nikita Chigvintsev <nikmail007@gmail.com>
2020-05-07 18:13:39 +0700

    Added error cases in stopWatch function

--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -1,5 +1,13 @@
 void stopWatch(int* t)
 {
+    if (t[0] < 0 || t[1] < 0 || t[2] < 0) {
+        t[0] = t[1] = t[2] = 0;
+        cout << "Error: negative time. Time reload." << endl;
+    }
+    if (t[0] > 60 || t[1] > 60 || t[2] > 24) {
+        t[0] = t[1] = t[2] = 0;
+        cout << "Error: Time out of range. Time reload." << endl;
+    }
     if (t[0] > 59) {
         t[1]++;
     }
