3054e65011bd9a65ed038581e094a6c104b5f9e0
Nikita Chigvintsev <nikmail007@gmail.com>
2020-05-26 11:15:21 +0700

    Added option to not enter a name

--- src/main.cpp
+++ src/main.cpp
@@ -66,15 +66,14 @@ int main()
         gameBoard[i] = new int[n];
     }
     generateArray(gameBoard, n);
-    cout << endl;
     int time[3];
     for (int i = 0; i < 3; i++) {
         time[i] = 0;
     }
-    bool timerStart = false;
-    bool isSolved = false;
-    bool showLeaderboard = false;
+    bool timerStart, isSolved, showLeaderboard, nameInput;
+    timerStart = isSolved = showLeaderboard = nameInput = false;
     while (window.isOpen()) {
+        ostringstream out;
         Vector2i pos = Mouse::getPosition(window);
         int dir = 0;
         int x = pos.x / 32;
@@ -85,15 +84,14 @@ int main()
             if (event.type == Event::Closed) {
                 window.close();
             }
-            if (event.type == Event::MouseButtonPressed) {
-                if (x == 5 && y == 4) {
-                    if (!showLeaderboard) {
-                        generateArray(gameBoard, n);
-                        clock.restart();
-                        timerStart = false;
-                        for (int i = 0; i < 3; i++) {
-                            time[i] = 0;
-                        }
+            if (event.type == Event::MouseButtonPressed && !nameInput
+                && !isSolved) {
+                if (x == 5 && y == 4 && !showLeaderboard) {
+                    generateArray(gameBoard, n);
+                    clock.restart();
+                    timerStart = false;
+                    for (int i = 0; i < 3; i++) {
+                        time[i] = 0;
                     }
                 } else if (x >= 1 && x <= 4 && y == 5) {
                     showLeaderboard = !showLeaderboard;
@@ -102,105 +100,123 @@ int main()
         }
         window.clear(Color::Black);
         if (isSolved) {
-            getResult(time, result, vector_result);
-            writeResult(result, vector_result);
-            readResult(result, vector_result);
-            generateArray(gameBoard, n);
-            clock.restart();
-            timerStart = false;
-            for (int i = 0; i < 3; i++) {
-                time[i] = 0;
-            }
+            cout << "Push the button (y/n) anywhere except console if you want "
+                    "to enter name for leaderboard"
+                 << endl;
+            nameInput = true;
             isSolved = false;
-        } else {
-            milliSecond = moveTimer.getElapsedTime().asMilliseconds();
-            if (timerStart) {
-                time[0] = clock.getElapsedTime().asSeconds();
-
-            } else {
+        }
+        if (nameInput) {
+            if (Keyboard::isKeyPressed(Keyboard::Y)) {
+                getResult(time, result, vector_result);
+                writeResult(result, vector_result);
+                readResult(result, vector_result);
+                generateArray(gameBoard, n);
                 clock.restart();
-            }
-            if (time[0] > 59) {
-                clock.restart();
-            }
-            stopWatch(time);
-            if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-                window.close();
-            }
-            if (!showLeaderboard) {
-                if ((Keyboard::isKeyPressed(Keyboard::A)
-                     || Keyboard::isKeyPressed(Keyboard::Left))
-                    && milliSecond > 250) {
-                    dir = 1;
-                    moveF(dir, gameBoard, n, emptyElem);
-                    moveTimer.restart();
-                    timerStart = true;
-                } else if (
-                        (Keyboard::isKeyPressed(Keyboard::D)
-                         || Keyboard::isKeyPressed(Keyboard::Right))
-                        && milliSecond > 250) {
-                    dir = 2;
-                    moveF(dir, gameBoard, n, emptyElem);
-                    moveTimer.restart();
-                    timerStart = true;
-                } else if (
-                        (Keyboard::isKeyPressed(Keyboard::W)
-                         || Keyboard::isKeyPressed(Keyboard::Up))
-                        && emptyElem[0] < 4 && milliSecond > 250) {
-                    dir = 3;
-                    moveF(dir, gameBoard, n, emptyElem);
-                    moveTimer.restart();
-                    timerStart = true;
-                } else if (
-                        (Keyboard::isKeyPressed(Keyboard::S)
-                         || Keyboard::isKeyPressed(Keyboard::Down))
-                        && emptyElem[0] > 1 && milliSecond > 250) {
-                    dir = 4;
-                    moveF(dir, gameBoard, n, emptyElem);
-                    moveTimer.restart();
-                    timerStart = true;
+                timerStart = false;
+                for (int i = 0; i < 3; i++) {
+                    time[i] = 0;
                 }
-                for (int i = 1; i < 5; i++) {
-                    for (int j = 1; j < 5; j++) {
-                        number.setTextureRect(
-                                IntRect(32 * gameBoard[j][i], 0, 32, 32));
-                        number.setPosition(32 * i, 32 * j);
-                        window.draw(number);
-                        if (gameBoard[j][i] == 0) {
-                            emptyElem[0] = j;
-                            emptyElem[1] = i;
+                nameInput = false;
+            } else if (Keyboard::isKeyPressed(Keyboard::N)) {
+                generateArray(gameBoard, n);
+                clock.restart();
+                timerStart = false;
+                for (int i = 0; i < 3; i++) {
+                    time[i] = 0;
+                }
+                nameInput = false;
+            }
+        } else {
+            if (showLeaderboard) {
+                readResult(result, vector_result);
+                for (int i = 0; i < int(vector_result.size()); i++) {
+                    out << i + 1 << ". " << vector_result[i].name << " - "
+                        << setfill('0') << setw(2) << vector_result[i].hours
+                        << ":" << setfill('0') << setw(2)
+                        << vector_result[i].minutes << ":" << setfill('0')
+                        << setw(2) << vector_result[i].seconds << '\n';
+                }
+                textLeaderboard.setString(out.str());
+                out.str("");
+                textLeaderboard.setPosition(20, 24);
+                window.draw(textLeaderboard);
+                vector_result.clear();
+            } else {
+                milliSecond = moveTimer.getElapsedTime().asMilliseconds();
+                if (timerStart) {
+                    time[0] = clock.getElapsedTime().asSeconds();
+
+                } else {
+                    clock.restart();
+                }
+                if (time[0] > 59) {
+                    clock.restart();
+                }
+                stopWatch(time);
+                if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+                    window.close();
+                }
+                if (!showLeaderboard) {
+                    if ((Keyboard::isKeyPressed(Keyboard::A)
+                         || Keyboard::isKeyPressed(Keyboard::Left))
+                        && milliSecond > 250) {
+                        dir = 1;
+                        moveF(dir, gameBoard, n, emptyElem);
+                        moveTimer.restart();
+                        timerStart = true;
+                    } else if (
+                            (Keyboard::isKeyPressed(Keyboard::D)
+                             || Keyboard::isKeyPressed(Keyboard::Right))
+                            && milliSecond > 250) {
+                        dir = 2;
+                        moveF(dir, gameBoard, n, emptyElem);
+                        moveTimer.restart();
+                        timerStart = true;
+                    } else if (
+                            (Keyboard::isKeyPressed(Keyboard::W)
+                             || Keyboard::isKeyPressed(Keyboard::Up))
+                            && emptyElem[0] < 4 && milliSecond > 250) {
+                        dir = 3;
+                        moveF(dir, gameBoard, n, emptyElem);
+                        moveTimer.restart();
+                        timerStart = true;
+                    } else if (
+                            (Keyboard::isKeyPressed(Keyboard::S)
+                             || Keyboard::isKeyPressed(Keyboard::Down))
+                            && emptyElem[0] > 1 && milliSecond > 250) {
+                        dir = 4;
+                        moveF(dir, gameBoard, n, emptyElem);
+                        moveTimer.restart();
+                        timerStart = true;
+                    }
+                    for (int i = 1; i < 5; i++) {
+                        for (int j = 1; j < 5; j++) {
+                            number.setTextureRect(
+                                    IntRect(32 * gameBoard[j][i], 0, 32, 32));
+                            number.setPosition(32 * i, 32 * j);
+                            window.draw(number);
+                            if (gameBoard[j][i] == 0) {
+                                emptyElem[0] = j;
+                                emptyElem[1] = i;
+                            }
                         }
                     }
+                    restart.setPosition(160, 128);
+                    window.draw(restart);
                 }
             }
+            isSolved = checkToWin(gameBoard, n);
+            out << setfill('0') << setw(2) << time[2] << ":" << setfill('0')
+                << setw(2) << time[1] << ":" << setfill('0') << setw(2)
+                << time[0];
+            textTimer.setString(out.str());
+            out.str("");
+            textTimer.setPosition(32, 0);
+            window.draw(textTimer);
+            highScore.setPosition(32, 160);
+            window.draw(highScore);
+            window.display();
         }
-        isSolved = checkToWin(gameBoard, n);
-        ostringstream out;
-        out << setfill('0') << setw(2) << time[2] << ":" << setfill('0')
-            << setw(2) << time[1] << ":" << setfill('0') << setw(2) << time[0];
-        textTimer.setString(out.str());
-        textTimer.setPosition(32, 0);
-        window.draw(textTimer);
-        out.str("");
-        if (!isSolved && showLeaderboard) {
-            readResult(result, vector_result);
-            for (int i = 0; i < int(vector_result.size()); i++) {
-                out << i + 1 << ". " << vector_result[i].name << " - "
-                    << setfill('0') << setw(2) << vector_result[i].hours << ":"
-                    << setfill('0') << setw(2) << vector_result[i].minutes
-                    << ":" << setfill('0') << setw(2)
-                    << vector_result[i].seconds << '\n';
-            }
-            textLeaderboard.setString(out.str());
-            textLeaderboard.setPosition(20, 24);
-            window.draw(textLeaderboard);
-            vector_result.clear();
-        } else {
-            restart.setPosition(160, 128);
-            window.draw(restart);
-        }
-        highScore.setPosition(32, 160);
-        window.draw(highScore);
-        window.display();
     }
 }
