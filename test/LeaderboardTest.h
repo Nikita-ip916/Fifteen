9cb21ee1826150769857a67b8ed5eec5cfc39f64
Nikita Chigvintsev <nikmail007@gmail.com>
2020-06-01 11:38:08 +0700

    Fixed init of vector results in LeaderboardTest.h

--- test/LeaderboardTest.h
+++ test/LeaderboardTest.h
@@ -12,24 +12,26 @@ struct Result {
 } result;
 
 class LeaderboardTest : public CxxTest::TestSuite {
-    vector<Result> expVectorResult
-            = {{"First", 0, 0, 50},
-               {"Second", 0, 0, 51},
-               {"Third", 0, 1, 50},
-               {"Fourth", 0, 1, 51},
-               {"Fifth", 1, 0, 50},
-               {"Sixth", 1, 0, 51}};
-    vector<Result> vectorResult
-            = {{"Sixth", 1, 0, 51},
-               {"Third", 0, 1, 50},
-               {"Fifth", 1, 0, 50},
-               {"First", 0, 0, 50},
-               {"Fourth", 0, 1, 51},
-               {"Second", 0, 0, 51}};
+    vector<Result> expVectorResult;
+    vector<Result> vectorResult;
 
 public:
     void testSortNoRepetition(void)
     {
+        expVectorResult
+                = {{"First", 0, 0, 50},
+                   {"Second", 0, 0, 51},
+                   {"Third", 0, 1, 50},
+                   {"Fourth", 0, 1, 51},
+                   {"Fifth", 1, 0, 50},
+                   {"Sixth", 1, 0, 51}};
+        vectorResult
+                = {{"Sixth", 1, 0, 51},
+                   {"Third", 0, 1, 50},
+                   {"Fifth", 1, 0, 50},
+                   {"First", 0, 0, 50},
+                   {"Fourth", 0, 1, 51},
+                   {"Second", 0, 0, 51}};
         int length = vectorResult.size();
         int* indexArray = new int[length];
         for (int i = 0; i < length; i++) {
